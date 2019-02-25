60d22689a6f7ee141e7bb7308941d0e99ed83534
AnushreeJoshi1994 <47772819+AnushreeJoshi1994@users.noreply.github.com>
2019-02-25 12:04:29 +0530

    Update demo_testcase.java

--- github_demo/src/github_demo/demo_testcase.java
+++ github_demo/src/github_demo/demo_testcase.java
@@ -1,6 +1,11 @@
 package github_demo;
 
 public class demo_testcase {
+	
+	static
+	{
+		System.setProperty("webdriver.chrome.driver","./driver.chromedriver.exe");
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
